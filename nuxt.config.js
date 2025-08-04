6c25f2bf2f1e213a5c211dcec46afe35da5b2619
hieunh03 <huyhieuUETK66@gmail.com>
2025-08-04 13:14:08 +0700

    add firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Target for static hosting
+  target: 'static',
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - frontend_kinobi',
